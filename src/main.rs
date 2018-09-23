0e3be0b06c6710502ae18473dd35e5e7746c4fcd
Richard Mitic <richard.h.mitic@gmail.com>
2018-09-23 22:29:39 +0200

    Add zooming

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,51 @@ use termion::input::TermRead;
 use termion::raw::{IntoRawMode};
 use termion::terminal_size;
 
+/// A view into a wave. Beginning and end as numbers between 0. and 1.
+type View = (f64, f64);
 
-fn print_wave_raw(core: &mut Core, width: &usize, height: &usize, start: &mut f32, end: &mut f32, screen: &mut Write) {
-    let peaks = core.get_peaks(*start, *end, *width as u32);
+struct ViewPoint {
+    begin: f64,
+    end: f64,
+    shift_delta: f64,
+    zoom_delta: f64
+}
+
+impl ViewPoint {
+    fn shift_right(&mut self) -> View {
+        self.begin += self.shift_delta;
+        self.end += self.shift_delta;
+        (self.begin, self.end)
+    }
+    
+    fn shift_left(&mut self) -> View {
+        self.begin -= self.shift_delta;
+        self.end -= self.shift_delta;
+        (self.begin, self.end)
+    }
+
+    fn zoom_in(&mut self) -> View {
+        let x = (self.end - self.begin) * self.zoom_delta; 
+        self.begin += x;
+        self.end -= x;
+        (self.begin, self.end)
+    }
+    
+    fn zoom_out(&mut self) -> View {
+        let x = (self.end - self.begin) * self.zoom_delta; 
+        self.begin -= x;
+        self.end += x;
+        (self.begin, self.end)
+    }
+
+    fn get_view(&mut self) -> View {
+        (self.begin, self.end)
+    }
+}
+
+
+fn print_wave_raw(core: &mut Core, width: &usize, height: &usize, view: View, screen: &mut Write) {
+    let peaks = core.get_peaks(view.0 as f32, view.1 as f32, *width as u32);
     let wave = core.draw_wave(peaks, &width, &height);
     print_pixels_raw(wave, screen);
 }
@@ -30,10 +72,6 @@ fn print_pixels_raw(wave: Vec<Vec<char>>, screen: &mut Write) {
     screen.flush().unwrap();
 }
 
-fn shift(begin: &mut f32, end: &mut f32, delta: &f32) {
-    *begin += delta;
-    *end += delta;
-}
 
 fn setup_screen(screen: &mut Write) {
     write!(screen, "{}{}", termion::clear::All, termion::cursor::Hide).unwrap();
@@ -77,42 +115,57 @@ fn main() {
         .arg(clap::Arg::with_name("shift")
             .short("s")
             .default_value("0.1")
-            .help("Amount to shift left and right"))
+            .help("Amount to shift left and right per key press"))
+        .arg(clap::Arg::with_name("zoom")
+            .short("z")
+            .default_value("0.1")
+            .help("Amount to zoom in and out per key press"))
         .get_matches();
 
     let size = terminal_size().unwrap();
 
     let filepath = matches.value_of("INPUT");
-    let mut beginning = matches.value_of("begin").unwrap().parse().unwrap();
-    let mut end = matches.value_of("end").unwrap().parse().unwrap();
     let width = matches.value_of("width").unwrap().parse().unwrap_or(size.0) as usize;
     let height = matches.value_of("height").unwrap().parse().unwrap_or(size.1) as usize;
-    let shift_delta: f32 = matches.value_of("shift").unwrap().parse().unwrap();
     
+    let mut view_point = ViewPoint {
+        begin: matches.value_of("begin").unwrap().parse().unwrap(),
+        end: matches.value_of("end").unwrap().parse().unwrap(),
+        shift_delta: matches.value_of("shift").unwrap().parse().unwrap(),
+        zoom_delta: matches.value_of("zoom").unwrap().parse().unwrap()
+    };
+
     println!("{:?}", filepath);
 
     let mut c = Core::new();
     c.load(filepath.unwrap().to_string());
-
-    //print_wave(&mut c, &width, &height, &mut beginning, &mut end);
 
     let stdin = stdin();
     let mut out = stdout().into_raw_mode().unwrap();
 
     setup_screen(&mut out);
 
-    print_wave_raw(&mut c, &width, &height, &mut beginning, &mut end, &mut out);
+    let view = view_point.get_view();
+    print_wave_raw(&mut c, &width, &height, view, &mut out);
 
     for key in stdin.keys() {
         match key.unwrap() {
             Key::Char('q') => break,
             Key::Left => {
-                shift(&mut beginning, &mut end, &shift_delta);
-                print_wave_raw(&mut c, &width, &height, &mut beginning, &mut end, &mut out);
+                let view = view_point.shift_left();
+                print_wave_raw(&mut c, &width, &height, view, &mut out);
             },
             Key::Right => {
-                shift(&mut beginning, &mut end, &(-shift_delta));
-                print_wave_raw(&mut c, &width, &height, &mut beginning, &mut end, &mut out);
+                let view = view_point.shift_right();
+                print_wave_raw(&mut c, &width, &height, view, &mut out);
+            },
+            Key::Up => {
+                let view = view_point.zoom_in();
+                print_wave_raw(&mut c, &width, &height, view, &mut out);
+            },
+            Key::Down => {
+                let view = view_point.zoom_out();
+                print_wave_raw(&mut c, &width, &height, view, &mut out);
             },
             _ => {}
         }
